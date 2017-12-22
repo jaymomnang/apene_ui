01dac10eb0672652f35174198c23364203c549e6
Jay Momnang <jaymomnang@users.noreply.github.com>
2017-12-22 19:10:38 +0100

    invoice details

--- controllers/InvoicesController.js
+++ controllers/InvoicesController.js
@@ -1,71 +1,69 @@
 'use strict';
 
 //load all invoices
-exports.list_all_invoices = function(req, res) {
+exports.list_all_invoices = function (req, res) {
 
   console.log(req.session.email);
-  if (req.session.email == undefined){
-    res.render("login");
-  }
-  else{
 
-    var auth_url = mc_api + "invoice/";
-    request(auth_url, function (error, response, body) {
 
-      if (error) return error;
-      var data = JSON.parse(body);
-      req.session.invoices = data;
+  var auth_url = mc_api + "invoice/";
+  request(auth_url, function (error, response, body) {
 
-      var ui_data = req.session;
-      res.render("invoices", {menus, ui_data});
+    if (error) return error;
+    var data = JSON.parse(body);
+    req.session.invoices = data;
 
-    });
-  }
+    var ui_data = req.session;
+    res.render("tables", { menus, ui_data });
+
+  });
+
 };
 
 //load blank invoice
-exports.load_blank = function(req, res) {
+exports.load_blank = function (req, res) {
 
-  console.log(req.session.email);
-  if (req.session.email == undefined){
-    res.render("login");
-  }
-  else{
+  console.log(req.session);
+  //if (req.session.email == undefined){
+  //  res.render("login");
+  //}
+  //else{
 
-    var ui_data = req.session;
-    res.render("invoice", {menus, ui_data});
-  }
+  var ui_data = req.session;
+  var hdr_label = "New Invoice";
+  res.render("invoice", { menus, ui_data, hdr_label });
+  //}
 };
 
 
 //post page data
-exports.add_invoice = function(req, res) {
+exports.add_invoice = function (req, res) {
 
-  if (req.session.email == undefined){
+  if (req.session.email == undefined) {
     res.render("login");
-  }else{
+  } else {
     var url_partial = "invoice/";
     var auth_url = mc_api + url_partial;
-    request.post({headers: {'content-type': 'application/x-www-form-urlencoded'}, url: auth_url, form:req.body }, function(error, response, body){
-        req.session.invoice = JSON.parse(body);
-        var msg = 'Error creating invoice, Please contact your administrator';
-        var failed = true;
-        if (!error){
-            failed = false;
-            msg = 'invoice successfully created';
-            var ui_data = req.session;
-            res.render('invoice', {menus, ui_data, failed, msg});
-        }
+    request.post({ headers: { 'content-type': 'application/x-www-form-urlencoded' }, url: auth_url, form: req.body }, function (error, response, body) {
+      req.session.invoice = JSON.parse(body);
+      var msg = 'Error creating invoice, Please contact your administrator';
+      var failed = true;
+      if (!error) {
+        failed = false;
+        msg = 'invoice successfully created';
+        var ui_data = req.session;
+        res.render('invoice', { menus, ui_data, failed, msg });
+      }
     });
   }
 };
 
 
-exports.get_invoice = function(req, res) {
+exports.get_invoice = function (req, res) {
 
-  if (req.session.email == undefined){
+  if (req.session.email == undefined) {
     res.render("login");
-  }else{
+  } else {
     var url_partial = "invoice/" + req.params.invoice_id;
     var auth_url = mc_api + url_partial;
 
@@ -73,21 +71,22 @@ exports.get_invoice = function(req, res) {
       var data = JSON.parse(body);
       //prepare display data
       //TODO: load the data needed here.
-      res.render(url_partial, {menus, data});
+      var hdr_label = "Invoice Details";
+      res.render(url_partial, { menus, data, hdr_label });
     });
   }
 
 };
 
-exports.update_invoice = function(req, res) {
+exports.update_invoice = function (req, res) {
 
-  if (req.session.email == undefined){
+  if (req.session.email == undefined) {
     res.render("login");
-  }else{
+  } else {
     var url_partial = "invoice/" + req.params.taskId;
     var auth_url = mc_api + url_partial;
     req.body.isActive = false;
-    request.put({headers: {'content-type': 'application/x-www-form-urlencoded'}, url: auth_url, form:req.body }, function (error, response, body) {
+    request.put({ headers: { 'content-type': 'application/x-www-form-urlencoded' }, url: auth_url, form: req.body }, function (error, response, body) {
       var data = JSON.parse(body);
       //prepare display data
       res.render(url_partial, data);
@@ -95,11 +94,11 @@ exports.update_invoice = function(req, res) {
   }
 };
 
-exports.delete_invoice = function(req, res) {
+exports.delete_invoice = function (req, res) {
   //TODO: write a process for deleting a task
-  if (req.session.email == undefined){
+  if (req.session.email == undefined) {
     res.render("login");
-  }else{
+  } else {
 
   }
 
